9d0aba979af563f6a04e2513fd3903f08e4d1de6
ramaputra-rgb <ramaputrasofyan@gmail.com>
2026-09-03 14:56:52 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Route untuk Halaman Login (Default Tampilan Pertama)
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->name('login');
+
+// Route Login Gambar 2 (Split Screen)
+Route::get('/login-b', function () {
+    return view('auth.login_b');
+})->name('login.b');
+
+// Route untuk Halaman Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
